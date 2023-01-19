a604f953ff941af5308ce4500eeebe2b2e406ff6
Ismael Amezcua <amezcua.ismael@gmail.com>
2023-01-19 12:15:36 -0600

    feat: update Next config to allow picsum.photos domain

--- components/featured.tsx
+++ components/featured.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import meta from "../lib/_meta";
 import { IconChevronRight } from "@tabler/icons";
 import Badge from "./badge";
@@ -10,12 +11,19 @@ export default function Featured() {
     <>
       <div className="group container my-16">
         <div className="grid grid-cols-1 md:grid-cols-2 md:gap-8">
-          <Link href={`/posts/${f.slug}`}>
-            <img
+          <Link href={`/posts/${f.slug}`} className="relative">
+            <div className="aspect-video relative">
+              <Image
+                src={f.image.url}
+                alt={f.image.alt}
+                fill
+              />
+            </div>
+            {/* <img
               src={f.image.url}
               alt={f.image.alt}
               className="aspect-[4/3] w-full object-cover"
-            />
+            /> */}
           </Link>
           <div className="flex flex-col justify-center">
             <div className="inline-flex space-x-4">
